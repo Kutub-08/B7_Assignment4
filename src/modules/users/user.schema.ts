c3d911504ff13aeae4f3bb619af751fb2b118c64
Kutub Uddin <kutubuddin2003251251@gmail.com>
2026-08-04 17:36:27 +0600

    sorted the src model

--- src/modules/users/user.schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const updateUserSchema = z.object({
-  name: z.string().min(2, "Name must be at least 2 characters").optional(),
-  phone: z.string().min(1, "Phone is required").optional(),
-});
