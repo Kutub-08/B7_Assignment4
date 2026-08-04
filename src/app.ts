441e7d58634891fd3491eb5c2361db48ffee6288
Kutub Uddin <kutubuddin2003251251@gmail.com>
2026-08-04 21:17:21 +0600

    fix error

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,7 @@ app.use(cookieParser())
 app.get("/",(req, res) => {
     res.json({
         message: "Hello welcome to my project",
-        author: "Masad Rayan"
+        author: "Kutub Uddin"
     })
 })
 
